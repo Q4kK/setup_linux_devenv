b3c3c5ebab1711c2d751ba0172c6699bd789544c
Q4kK <quantumstar4k@gmail.com>
2023-03-31 12:40:45 -0500

    Fix: added ssh agent and PID
    The ssh agent was not initialized or detecting the keys, so a new
    function was added to initilize the agent and pass the information to
    the ssh generation function. Additionally, a bug with the home directory
    was fixed to properly generate the home path.

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 git_user = "godot"
 git_email = "125313163+Q4kK@users.noreply.github.com"
-home_dir = os.getenv("HOME")
+home_dir = os.path.expanduser('~')
 
 packages = ["git", "ansible", "curl", "snap", "openssh-server", "python3-pip"]
--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 import subprocess
-import sh
 import config
-
+import os
+import re
 
 def install_packages(pkg):
     command = ["sudo", "apt", "install", "-y"]
     command.extend(pkg)
     return command
 
+def install_requirements():
+    command = ["pip", "install", "-r", "requirements.txt"]
+    return command
+
+subprocess.call(install_requirements())
+import sh
 
 def packages():
     return config.packages
@@ -17,20 +23,39 @@ def vscode_install():
     sh.sudo.snap("install", "--classic", "code")
     print("[debug]: installing vscode...")
 
-
 def git_config():
     sh.git("config", "--global", "user.name", config.git_user)
     sh.git("config", "--global", "user.email", config.git_email)
     print("[debug]: Changing user configs...")
 
+def start_agent():
+    command = ["ssh-agent", "-s"]
+    output = subprocess.Popen(command, stdout=subprocess.PIPE)
+    output.wait()
+
+    ssh_stdout = output.stdout.readlines()
+    ssh_stdout = "".join([i.decode() for i in ssh_stdout])
+
+    SSH_AGENT_PID=re.search(r"SSH_AGENT_PID=([0-9]*)", ssh_stdout).group(1)
+    SSH_AUTH_SOCK=re.search(r"SSH_AUTH_SOCK=(.*);[ ]", ssh_stdout).group(1)
+    os.environ['SSH_AGENT_PID'] = SSH_AGENT_PID
+    os.environ['SSH_AUTH_SOCK'] = SSH_AUTH_SOCK
+
 
 def ssh_key_generate():
-    if sh.ssh_add("-L") == "":
-        sh.ssh_keygen("-t", "ed25519", "-f", config.home_dir)
-    else:
+    start_agent()
+    sh.ssh_add()
+    try:
+        sh.ssh_add("-L")
         print("[debug]: Already generated ssh keys, skipping.")
-        pass
+    except:
+        print("[debug]: Generating new ssh keys.")
+        sh.ssh_keygen("-t", "ed25519", "-f", config.home_dir, "-q", "-N", "")
 
+
+def install_requirements():
+    command = ["pip", "install", "-r", "requirements.txt"]
+    return command
 
 subprocess.call(install_packages(packages()))
 ssh_key_generate()
